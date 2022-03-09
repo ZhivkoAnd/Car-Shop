dddc03bb1f8487474700bba5e33785d04238efc8
Zhivko Andreev <zhia@yousee.dk>
2022-03-09 14:15:06 +0100

    qwe

---
+++ src/Pages/ChooseCar.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import carList from "../components/CarList";
+import CarGrid from "../components/CarGrid";
+import NavMenu from "../components/UI/NavMenu";
+import Footer from "../components/UI/Footer";
+import Title from "../components/UI/Title";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import Background_PC_208 from "../Media/Background_PC_208.jpg";
+import Background_Mobile_208 from "../Media/Background_PC_208.jpg";
+// import { useCarContext } from "./components/CarContext";
+
+const ChooseCar = () => {
+  const [carData, setCarData] = useState(carList);
+  // const { counter, increaseCount } = useCarContext();
+
+  const filterCarBrand = (brand) => {
+    if (brand === "All") {
+      setCarData(carList);
+      return;
+    }
+    const filteredCarsBrand = carList.filter((car) => car.brand === brand);
+    setCarData(filteredCarsBrand);
+  };
+
+  const filterCarType = (type) => {
+    if (type === "All") {
+      setCarData(carList);
+      return;
+    }
+    const filteredCarsType = carList.filter((car) => car.type === type);
+    setCarData(filteredCarsType);
+  };
+
+  return (
+    <BackgroundContainer>
+      <NavMenu filterCarBrand={filterCarBrand} filterCarType={filterCarType}/>
+      <Title />
+      <CarGrid cars={carData} />
+      <Footer />
+    </BackgroundContainer>
+  );
+};
+
+export default ChooseCar;
+
+const BackgroundContainer = styled.div`
+  margin: 0px;
+  background-repeat: no-repeat fixed;
+  background-color: black;
+  background-position: center;
+  width: 100vw;
+  height: 100vh;
+  transition: 0.5s;
+  background-size: cover;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  background-image: url(${Background_PC_208});
+`;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,43 +1,19 @@
-import React, { useState } from "react";
-import carList from "../components/CarList";
-import CarGrid from "../components/CarGrid";
-import NavMenu from "../components/UI/NavMenu";
-import Footer from "../components/UI/Footer";
+import React from "react";
 import Title from "../components/UI/Title";
 import { motion } from "framer-motion";
 import styled from "styled-components";
 import Background_PC_208 from "../Media/Background_PC_208.jpg";
 import Background_Mobile_208 from "../Media/Background_PC_208.jpg";
-// import { useCarContext } from "./components/CarContext";
+import ExploreButton from '../components/UI/ExploreButton'
+import Subtitle from "../components/UI/Subtitle";
 
 const Home = () => {
-  const [carData, setCarData] = useState(carList);
-  // const { counter, increaseCount } = useCarContext();
-
-  const filterCarBrand = (brand) => {
-    if (brand === "All") {
-      setCarData(carList);
-      return;
-    }
-    const filteredCarsBrand = carList.filter((car) => car.brand === brand);
-    setCarData(filteredCarsBrand);
-  };
-
-  const filterCarType = (type) => {
-    if (type === "All") {
-      setCarData(carList);
-      return;
-    }
-    const filteredCarsType = carList.filter((car) => car.type === type);
-    setCarData(filteredCarsType);
-  };
 
   return (
     <BackgroundContainer>
-      <NavMenu filterCarBrand={filterCarBrand} filterCarType={filterCarType}/>
       <Title />
-      <CarGrid cars={carData} />
-      <Footer />
+      <Subtitle/>
+      <ExploreButton/>
     </BackgroundContainer>
   );
 };
