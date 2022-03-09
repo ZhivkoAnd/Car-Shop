2f3243834e8b185d44e3a1f092041c5c7c7202d4
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2022-03-09 19:32:58 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import ChooseCar from "./Pages/ChooseCar";
+import AboutUs from "./Pages/AboutUs";
+import Contacts from "./Pages/Contacts";
 import "./Styles/styles.css";
 
 const App = () => {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/choosecar" element={<ChooseCar />}></Route>
+          <Route exact path="/aboutus" element={<AboutUs />}></Route>
+          <Route exact path="/contacts" element={<Contacts />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
