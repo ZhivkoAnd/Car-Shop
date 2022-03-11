f76a0ba628db1b3a5d5c51102241de8d7015fad9
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2022-03-11 11:13:25 +0100

    asd

--- src/components/UI/ExploreButton.js
+++ src/components/UI/ExploreButton.js
@@ -1,14 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const ExploreButton = ({ button }) => {
   return (
-    <ExploreButtonWrapper>
-      <Link to="/ChooseCar">
-        <div className="exploreButton">{button}</div>
-      </Link>
-    </ExploreButtonWrapper>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, delay: 0.4 }}
+    >
+      <ExploreButtonWrapper>
+        <Link to="/ChooseCar">
+          <div className="exploreButton">{button}</div>
+        </Link>
+      </ExploreButtonWrapper>
+    </motion.div>
   );
 };
 
@@ -18,7 +25,7 @@ const ExploreButtonWrapper = styled.div`
   .exploreButton {
     padding: 1rem 9rem 1rem 9rem;
     letter-spacing: 0.2rem;
-    border: 2px solid rgb(90 3 0);
+    border: 1px solid rgb(255 255 255);
     border-radius: 10px;
     background: rgba(0, 0, 0, 0.4);
     color: #fdfdfc;
--- src/components/UI/NavMenu.js
+++ src/components/UI/NavMenu.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 import FilterCars from "../FilterCars";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavMenu = ({ filterCarBrand, filterCarType }) => {
   return (
     <NavWrapper>
-      <FilterCars filterCarBrand={filterCarBrand} filterCarType = {filterCarType}/>
+      <FilterCars
+        filterCarBrand={filterCarBrand}
+        filterCarType={filterCarType}
+      />
     </NavWrapper>
   );
 };
